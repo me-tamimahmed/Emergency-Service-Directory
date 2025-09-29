b94743d688979ec1795aa3323530d3d619f82769
me-tamimahmed <me.tamimahmed01@gmail.com>
2025-09-29 15:21:12 +0600

    my-work-3

---
+++ node.js
@@ -0,0 +1,22 @@
+// step 01
+// Navbar heart counter
+let heartCount = 0;
+const heartCounter = document.getElementById("add-heart");
+
+
+const hearts = document.querySelectorAll(".card-heart");
+
+hearts.forEach(heart => {
+    heart.addEventListener("click", () => {
+        heartCount++;
+        heartCounter.textContent = heartCount;
+
+       
+        heart.classList.toggle("fa-solid");  
+        heart.classList.toggle("fa-regular"); 
+        heart.style.color = heart.classList.contains("fa-solid") ? "red" : "";
+    });
+});
+
+
+// step 02
